bd59a26505c401b0190f178ee0092f8c639ca562
Rizky Afrianto <rizky.afrianto039@gmail.com>
2023-04-16 17:53:08 +0700

    Make View register and login, add auth for logic bagian 2

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+
+    <style>
+        .form-signin {
+            max-width: 330px;
+            padding: 15px;
+        }
+
+        .form-signin .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-signin input[type="email"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-signin input[type="password"] {
+            margin-bottom: 10px;
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+    </style>
+
+    <title>Space</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand fs-4" href="#"><i class="bi bi-medium"></i> Space</a>
+            <div class="navbar-nav">
+                <a class="nav-link" href="/register">Get Started</a>
+            </div>
+        </div>
+    </nav>
+
+    <main class="form-signin w-100 mx-auto mt-5">
+        <form action="/login" method="post">
+            <h1 class="h3 mb-3 fw-normal text-center mt-5">Welcome back</h1>
+
+            <div class="form-floating my-2">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                    id="email" placeholder="Email" required>
+                <label for="email">Email address</label>
+                @error('email')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="form-floating my-2">
+                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                <label for="password">Password</label>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-success mt-3" type="submit">Sign in</button>
+        </form>
+
+        {{-- click and redirect to regist page --}}
+        <small class="d-block text-center mt-2">Not already have accout?
+            <a href="/register">
+                Register now
+            </a>
+        </small>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
